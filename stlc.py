a0adf55db7291fc3aa5fdb4dd802384f90c15823
Evghenii <egx@egx-mbp.local>
2020-10-04 10:33:24 -0700

    working on python version

--- stlc.py
+++ stlc.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 import typing as ty
-from typing import Any as TAny, Callable as TLam
+from typing import Any as TAny, Callable as TLam, List as TList, \
+                   Dict as TDict
 
 AbstractF = ty.TypeVar('AbstractF', bound=TLam[..., TAny])
 def abstract(cls : AbstractF) -> TAny:
@@ -18,54 +19,54 @@ class TermI:
     __slots__ = ['_']
 
 class Ann(TermI):
-    __slots__ = ['term', 'type']
-    def __init__(self, term : TermC, type_ : Type):
+    __slots__ = ['e', 't']
+    def __init__(self, expr : TermC, type_ : Type):
         super().__init__()
-        self.term = term
-        self.type = type_
+        self.e = expr
+        self.t = type_
     def __repr__(self) -> str:
-        return f"{self.term}::{self.type}"
+        return f"{self.e}::{self.t}"
 
 class Bound(TermI):
-    __slots__ = ['pos']
+    __slots__ = ['i']
     def __init__(self, pos : int):
         super().__init__()
-        self.pos = pos
+        self.i = pos
     def __repr__(self) -> str:
-        return f"(Bound {self.pos})"
+        return f"(Bound {self.i})"
 
 class Free(TermI):
-    __slots__ = ['name']
+    __slots__ = ['x']
     def __init__(self, name : Name):
-        self.name = name
+        self.x = name
     def __repr__(self) -> str:
-        return f"(Free {self.name})"
+        return f"(Free {self.x})"
 
 class App(TermI):
-    __slots__ = ['termFun','termArg']
+    __slots__ = ['e','ep']
     def __init__(self, termFun : TermI, termArg : TermC):
-        self.termFun = termFun
-        self.termArg = termArg
+        self.e = termFun
+        self.ep = termArg
     def __repr__(self) -> str:
-        return f"{self.termFun}@{self.termArg}"
+        return f"{self.e}@{self.ep}"
 
 
 @abstract
 class TermC:
     __slots__ = ['_']
 class Inf(TermC):
-    __slots__ = ['term']
-    def __init__(self, term : TermI):
+    __slots__ = ['i']
+    def __init__(self, iterm : TermI):
         super().__init__()
-        self.term = term
+        self.i = iterm
     def __repr__(self) -> str:
-        return f"(Inf {self.term})"
+        return f"(Inf {self.i})"
 class Lam(TermC):
-    __slots__ = ['term']
-    def __init__(self, term : TermC):
-        self.term = term
+    __slots__ = ['e']
+    def __init__(self, expr : TermC):
+        self.e = expr
     def __repr__(self) -> str:
-        return f"(Lam {self.term})"
+        return f"(Lam {self.e})"
 
 @abstract
 class Name(): __slots__ = ['_']
@@ -78,11 +79,11 @@ class Global(Name):
         return f"(Global {self.str})"
 
 class Local(Name):
-    __slots__ = ['pos']
+    __slots__ = ['i']
     def __init__(self, pos : int):
-        self.pos = pos
+        self.i = pos
     def __repr__(self) -> str:
-        return f"(Local {self.pos})"
+        return f"(Local {self.i})"
 
 class Quote(Name):
     __slots__ = ['pos']
@@ -95,36 +96,36 @@ class Quote(Name):
 class Type: __slots__ = ['_']
 
 class TFree(Type):
-    __slots__ = ['name']
+    __slots__ = ['x']
     def __init__(self, name : Name):
-        self.name = name
+        self.x = name
     def __repr__(self) -> str:
-        return f"(TFree {self.name})"
+        return f"(TFree {self.x})"
 
 class Fun(Type):
-    __slots__ = ['typePrm','typeRes']
+    __slots__ = ['k','kp']
     def __init__(self, typePrm : Type, typeRes : Type):
-        self.typePrm = typePrm
-        self.typeRes = typeRes
+        self.t = typePrm
+        self.tp = typeRes
     def __repr__(self) -> str:
-        return f"{self.typePrm} -> {self.typeRes}"
+        return f"{self.t} -> {self.tp}"
 
 @abstract
 class Value: pass
 
 class VLam(Value):
-    __slots__ = ['lam']
+    __slots__ = ['f']
     def __init__(self, lam : TLam[[Value], Value]):
-        self.lam = lam
+        self.f = lam
     def __repr__(self) -> str:
-        return f"(VLam {self.lam})"
+        return f"(VLam {self.f})"
 
 class VNeutral(Value):
-    __slots__ = ['neutral']
+    __slots__ = ['n']
     def __init__(self, neutral : Neutral):
-        self.neutral = Neutral
+        self.n = neutral
     def __repr__(self) -> str:
-        return f"(VNeutral {self.neutral})"
+        return f"(VNeutral {self.n})"
 
 @abstract
 class Neutral: pass
@@ -132,14 +133,110 @@ class Neutral: pass
 class NFree(Neutral):
     __slots__ = ['name']
     def __init__(self, name : Name):
-        self.name = name
+        self.x = name
     def __repr__(self) -> str:
-        return f"(NFree self.name)"
+        return f"(NFree self.x)"
 class NApp(Neutral):
-    __slots__ = ['neutral', 'value']
+    __slots__ = ['n', 'v']
     def __init__(self, neutral : Neutral, value : Value):
-        self.neutral = neutral
-        self.value = neutral
+        self.n = neutral
+        self.v = neutral
     def __repr__(self) -> str:
-        return f"(NApp self.neutral self.value)"
+        return f"(NApp self.n self.v)"
+
+vfree : TLam[[Name],Value] = lambda n :  VNeutral(NFree(n))
+
+Env = TList[Value]
+
+def evalI(term : TermI, env : Env) -> Value:
+    if isinstance(term, Ann):
+        return evalC(term.e, env)
+    elif isinstance(term, Free):
+        return vfree(term.x)
+    elif isinstance(term, Bound):
+        return env[term.i]
+    elif isinstance(term, App):
+        return vapp(evalI(term.e, env), evalC(term.ep, env))
+    raise TypeError(f"Unknown instance '{type(term)}'")
+
+def vapp(v : Value, v1 : Value) -> Value:
+    if isinstance(v, VLam):
+        return v.f(v1)
+    elif isinstance(v, VNeutral):
+        return VNeutral(NApp(v.n, v1))
+    raise TypeError(f"Unknown instance '{type(v)}'")
+
+def evalC(term : TermC, env : Env) -> Value:
+    if isinstance(term, Inf):
+        return evalI(term.i, env)
+    elif isinstance(term, Lam):
+        lam_expr = term.e
+        return VLam(lambda x : evalC(lam_expr, [x] + env))
+    raise TypeError(f"Unknown instance '{type(term)}'")
+
+@abstract
+class Kind: pass
+class Star(Kind):
+    __slots__ = ['_']
+    def __repr__(self) -> str:
+        return f"Star"
+
+@abstract
+class Info: pass
+class HasKind(Info):
+    __slots__ = ['kind']
+    def __init__(self, kind : Kind):
+        self.kind = kind
+    def __repr__(self) -> str:
+        return f"(HasKind {self.kind})"
+class HasType(Info):
+    __slots__ = ['t']
+    def __init__(self, type_ : Type):
+        self.t = type_
+    def __repr__(self) -> str:
+        return f"(HasType {self.t})"
+
+Context = TDict[Name, Info]
+
+def kindC(c : Context, term : Type, k : Kind) -> None:
+    assert(isinstance(k, Star))
+    if isinstance(term, TFree):
+        if isinstance(c[term.x], HasKind):
+            return
+        else:
+            raise RuntimeError(f"unknown var identifier '{term.x}'")
+    elif isinstance(term, Fun):
+        kindC(c, term.t, Star())
+        kindC(c, term.tp, Star())
+    raise TypeError(f"Unknown instance '{type(term)}'")
+
+def typeI(i : int, c : Context, term : TermI) -> Type:
+    if isinstance(term, Ann):
+        kindC(c, term.t, Star())
+        typeC(i, c, term.e, term.t)
+        return term.t
+    elif isinstance(term, TFree):
+        info = c[term.x]
+        if isinstance(info, HasType):
+            return info.t
+        else:
+            raise RuntimeError(f"unknown type identifier '{term.x}'")
+    raise TypeError(f"Unknown instance '{type(term)}'")
+
+def dict_merge(a : TDict[TAny, TAny],
+               b : TDict[TAny, TAny]) -> TDict[TAny, TAny]:
+    a = a.copy()
+    a.update(b)
+    return a
+
+def typeC(i : int, c: Context, term : TermC, ty : Type) -> None:
+    if isinstance(term, Inf):
+        ty1 = typeI(i, c, term.i)
+        if ty != ty1:
+            raise TypeError(f"type mismatch: {ty} != {ty1}")
+        return
+    elif isinstance(term, Lam) and isinstance(ty, Fun):
+        typeC(i+1, dict_merge({Local(i): HasType(ty.t)}, c),
+                   substC(0, Free(Local(i)), term.e), ty.tp)
+    raise TypeError(f"Type misamtch: term={term}', type={type}")
 
