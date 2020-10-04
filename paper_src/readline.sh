53e39d2a7456f18c9ddf17894dbacb262aa7183f
Evghenii <egx@egx-mbp.local>
2020-10-04 11:15:27 -0700

    wip

---
+++ paper_src/readline.sh
@@ -0,0 +1,1 @@
+cabal install readline --extra-include-dirs=/usr/local/Cellar/readline/8.0.4/include/ --extra-lib-dirs=/usr/local/Cellar/readline/8.0.4/lib/ --configure-option=--with-readline-includes=/usr/local/Cellar/readline/8.0.4/include/ --configure-option=--with-readline-libraries=/usr/local/Cellar/readline/8.0.4/lib/
