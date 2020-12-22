5b241ca5788287977a11c480583ab2ce8ecc1c64
Evghenii <egx@egx-mbp.local>
2020-12-22 14:12:00 -0800

    wip

--- lampi/src/main.rs
+++ lampi/src/main.rs
@@ -1,4 +1,57 @@
 fn main() {
     println!("answer= {}", lampi::stlc::test());
     println!("Hello, world!");
+
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    struct T(Rc<dyn Fn(i32)->i32>);
+    impl T {
+        fn new(f : Rc<dyn Fn(i32)->i32>) -> T {
+            T(f)
+        }
+        fn clone(&self) -> T {
+            T::new(Rc::clone(&self.0))
+        }
+    }
+
+    let a = T::new(Rc::new(|x| x+3));
+    println!("count= {}", Rc::strong_count(&a.0));
+    let b = a.clone();
+    println!("count= {}", Rc::strong_count(&b.0));
+
+    #[derive(Clone, Debug)]
+    struct U(Rc<i32>);
+
+    let a = U(Rc::new(5));
+    println!("count= {}", Rc::strong_count(&a.0));
+    let b = a.clone();
+    println!("count= {}", Rc::strong_count(&a.0));
+    println!("count= {}", Rc::strong_count(&b.0));
+    let c = b.clone();
+    println!("count= {}", Rc::strong_count(&a.0));
+    println!("count= {}", Rc::strong_count(&b.0));
+    println!("count= {}", Rc::strong_count(&c.0));
+    println!("count= {:?} {:?} {:?}", a,b,c);
+
+    #[derive(Clone, Debug)]
+    struct V(Rc<RefCell<i32>>);
+
+    let a = V(Rc::new(RefCell::new(5)));
+    println!("count= {}", Rc::strong_count(&a.0));
+    let b = a.clone();
+    println!("count= {}", Rc::strong_count(&a.0));
+    println!("count= {}", Rc::strong_count(&b.0));
+    let c = b.clone();
+    println!("count= {}", Rc::strong_count(&a.0));
+    println!("count= {}", Rc::strong_count(&b.0));
+    println!("count= {}", Rc::strong_count(&c.0));
+    println!("count= {:?} {:?} {:?}", a,b,c);
+
+    *b.0.borrow_mut() = 42;
+    println!("count= {}", Rc::strong_count(&a.0));
+    println!("count= {}", Rc::strong_count(&b.0));
+    println!("count= {}", Rc::strong_count(&c.0));
+    println!("count= {:?} {:?} {:?}", a,b,c);
+
 }
