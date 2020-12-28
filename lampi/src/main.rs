957041aa565de84d13b65542562bb064de479817
Evghenii <egx@egx-mbp.local>
2020-12-28 06:44:19 -0800

    added e35 example

--- lampi/src/main.rs
+++ lampi/src/main.rs
@@ -170,32 +170,20 @@ fn main() {
         println!("qqeval(term2)= {:?}", quote0(&evalI(&term2, &Env::new())));
         println!("type(term1)= {:?}", typeI0(&env1, &term1));
         println!("type(term2)= {:?}", typeI0(&env2, &term2));
-        /*
-        id_ = Lam(Inf(Bound(0)))
-        const_ = Lam(Lam(Inf(Bound(1))))
-        free: TLam[[str], TermC] = lambda x: Inf(Free(Global(x)))
-        pi: TLam[[TermC, TermC], TermC] = lambda x, y: Inf(Pi(x, y))
-        term1 = App(Ann(id_, (pi(free("a"), free("a")))), free("y"))
-        term2 = App(
-            App(
-                Ann(
-                    const_,
-                    pi(pi(free("b"), free("b")), pi(free("a"), pi(free("b"), free("b")))),
-                ),
-                id_,
-            ),
-            free("y"),
-        )
-        env1: Context = {Global("y"): VNeutral(NFree(Global("a"))), Global("a"): VStar()}
-        env2 = env1.copy()
-        env2.update({Global("b"): VStar()})
 
-        print("eval(term1)=", evalI(term1, []))
-        print("qeval(term1)=", quote0(evalI(term1, [])))
-        print("qqeval(term2)=", quote0(evalI(term2, [])))
-        print("type(term1)=", typeI0(env1, term1))
-        print("type(term2)=", typeI0(env2, term2))
-                 */
+        // example for the following concrete syntax
+        // > let id = (\a x -> x) :: Pi (a :: *).a -> a
+        // id :: Pi (x::*) (y::x).x
+        let e35 = Ann(
+            box Lam(box Lam(box Inf(box Bound(0)))),
+            box pi(
+                box Inf(box Star),
+                box pi(box Inf(box Bound(0)), box Inf(box Bound(1))),
+            ),
+        );
+        println!("e35= {:?}", e35);
+        /*
+         */
     }
 
     {
